5fe4cd3dfe2b4f0b365ed8c20ca43068ade6730c
lfx <lifengxiang@kylinos.cn>
2025-03-06 16:42:52 +0800

    cpmlete-conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,34 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let default_person = Person::default();
+        if s.is_empty() {
+            return default_person;
+        }
+
+        let parts: Vec<&str> = s.split(',').collect();
+
+        if parts.len() != 2 {
+            return default_person;
+        }
+
+        let name = parts[0].to_string();
+        if name.is_empty() {
+            return default_person;
+        }
+
+        let age = parts[1].trim().parse::<usize>();
+        if age.is_err() {
+            return Person::default();
+        }
+
+        Person {
+            name: name.to_string(),
+            age: age.unwrap(),
+        }
     }
 }
 
